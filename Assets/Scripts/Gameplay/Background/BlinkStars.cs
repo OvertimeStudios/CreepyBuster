44ed11182c16d46354cda9ee321d964941dbbef6
nimdanet <jj.nimdnet@gmail.com>
2015-09-09 14:30:29 -0300

    - Background stars now fade in and out

---
+++ Assets/Scripts/Gameplay/Background/BlinkStars.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkStars : MonoBehaviour 
+{
+	public RandomBetweenTwoConst visibleTime;
+	public float fadeoutTime;
+	public RandomBetweenTwoConst invisibleTime;
+	public float fadeinTime;
+
+	private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sprite = GetComponent<SpriteRenderer>();
+
+		StartCoroutine(Blink());
+	}
+
+	private IEnumerator Blink()
+	{
+		//visible
+		Debug.Log("Visible: " + Time.time);
+		yield return new WaitForSeconds(visibleTime.Random());
+
+		//fade out
+		Debug.Log("Fade Out: " + Time.time);
+		while(sprite.color.a > 0)
+		{
+			Color c = sprite.color;
+			c.a -= Time.deltaTime / fadeoutTime;
+			sprite.color = c;
+
+			yield return null;
+		}
+
+		//invisible
+		Debug.Log("Invisible: " + Time.time);
+		yield return new WaitForSeconds(invisibleTime.Random());
+		
+		//fade in
+		Debug.Log("Fade In: " + Time.time);
+		while(sprite.color.a < 1)
+		{
+			Color c = sprite.color;
+			c.a += Time.deltaTime / fadeinTime;
+			sprite.color = c;
+			
+			yield return null;
+		}
+
+		StartCoroutine(Blink());
+	}
+}
