739b70e8e4dd4176e5e03f3dbe8fe2aa6ecdd1db
nimdanet <jj.nimdnet@gmail.com>
2015-10-09 17:37:32 -0300

    - Menu stars + parallax

---
+++ Assets/Scripts/Menu/StarsParallax.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarsParallax : MonoBehaviour 
+{
+	public Transform reference;
+	public float parallax;
+
+	private float initialPosition;
+	private float initialReferencePosition;
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myTransform = transform;
+		initialReferencePosition = reference.position.x;
+		initialPosition = myTransform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 pos = myTransform.position;
+		pos.x = initialPosition + ((reference.position.x - initialReferencePosition) * parallax);
+		myTransform.position = pos;
+	}
+}
